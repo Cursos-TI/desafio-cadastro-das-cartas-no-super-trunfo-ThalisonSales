14c0e006e7658b1ce250d716dcdd74641127e18e
ThalisonSales <thalisonsales03@gmail.com>
2025-01-08 19:40:00 +0000

    Adicionado densidade populacional e PIB per capita

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,29 +4,35 @@ int main() {
 char cod_carta [4], estado [20], cidade [20]; // Código da carta, nome do estado e nome da cidade.
 int populacao, pontos_tur; //População total e quantidade de pontos turísticos.
 float area, PIB; // Área em km² e PIB (Em letras maiúsculas).
+float densidade_pop, PIB_percapita; //Densidade populacional e PIB per capita.
 
-printf("Insira o codigo da carta:");
+printf("Insira o código da carta:");
 scanf(" %s", cod_carta);
 printf("Digite o nome do estado:");
 scanf(" %s", estado);
 printf("Digite o nome da cidade:");
 scanf(" %s", cidade);
-printf("Digite a quantidade populacional da cidade:");
-scanf("%d", &populacao);
 printf("Digite a quantidade de pontos turisticos da cidade:");
 scanf("%d", &pontos_tur);
-printf("Digite a area total em quilometros quadrados:");
+printf("Digite a quantidade populacional da cidade:");
+scanf("%d", &populacao);
+printf("Digite a área total em km²:");
 scanf("%f", &area);
 printf("Digite o PIB:");
 scanf("%f", &PIB);
 
+densidade_pop = populacao/area;
+PIB_percapita = PIB/populacao;
+
 printf("\nOs dados informados da carta %s sao:\n", cod_carta);
 printf("Estado: %s\n", estado);
-printf("Cidade: %s.\n", cidade);
-printf("População: %d.\n", populacao);
+printf("Cidade: %s\n", cidade);
 printf("Pontos turísticos: %d\n", pontos_tur);
-printf("Área total: %.3f.km²\n", area);
-printf("PIB: R$ %.2f.", PIB);
+printf("População: %d\n", populacao);
+printf("Área total: %.3f km²\n", area);
+printf("Densidade populacional: %.1f habitantes por km²\n", densidade_pop);
+printf("PIB: R$ %.2f\n", PIB);
+printf("PIB per capita: R$ %.3f\n", PIB_percapita);
 
 return 0;
 }
